4973b2e1d1cf8ee239613e96c5ce9edba958de7b
jedi24garcia <jedi24garcia@gmail.com>
2024-04-04 11:26:56 +1300

    first test failed

--- test.py
+++ test.py
@@ -4,3 +4,8 @@ import BowlingGame
 class TestBowlingGame(unittest.TestCase):
     def setUp(self):
         self.game = BowlingGame.BowlingGame()
+    
+    def testGutterGame(self):
+        for i in range(0, 20):
+            self.game.rolls(0)
+        assert self.game.score()==0
