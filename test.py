0d9cc1ccba6feb3026e9bbb37ab1e1b4ba4bf244
jedi24garcia <jedi24garcia@gmail.com>
2024-04-05 09:57:18 +1300

    second test errors

--- test.py
+++ test.py
@@ -21,3 +21,19 @@ class TestBowlingGame(unittest.TestCase):
 #   File "/Users/Jedi/Desktop/Project_IT6039/test.py", line 10, in testGutterGame
 #     self.game.rolls(0)
 # TypeError: 'list' object is not callable
+    def testAllOnes(self):
+#         test.TestBowlingGame.testAllOnes failed with error: <class 'AttributeError'> 'TestBowlingGame' object has no attribute 'rollMany'
+# Traceback (most recent call last):
+#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 57, in testPartExecutor
+#     yield
+#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 623, in run
+#     self._callTestMethod(testMethod)
+#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 579, in _callTestMethod
+#     if method() is not None:
+#        ^^^^^^^^
+#   File "/Users/Jedi/Desktop/Project_IT6039/test.py", line 25, in testAllOnes
+#     self.rollMany(1, 20)
+#     ^^^^^^^^^^^^^
+# AttributeError: 'TestBowlingGame' object has no attribute 'rollMany'
+        self.rollMany(1, 20)
+        assert self.game.score()==20
