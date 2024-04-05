b1908f8855a2c8d76ed4a3ff9c6673a5f1b44c37
jedi24garcia <jedi24garcia@gmail.com>
2024-04-05 16:02:23 +1300

    removed comments

--- test.py
+++ test.py
@@ -3,59 +3,18 @@ from BowlingGame import BowlingGame
 
 class TestBowlingGame(unittest.TestCase):
     def setUp(self):
-        # self.game = BowlingGame.BowlingGame()
         self.game = BowlingGame() 
         self.pin = 0
 
     def testGutterGame(self): 
-        # for i in range(0, 20):
-        #     self.game.rolls(0)
-        # assert self.game.score()==0
         self.rollMany(0, 20)
         self.assertEqual(self.game.score(), 0)
 
-# test.TestBowlingGame.testGutterGame failed with error: <class 'TypeError'> 'list' object is not callable
-# Traceback (most recent call last):
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 57, in testPartExecutor
-#     yield
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 623, in run
-#     self._callTestMethod(testMethod)
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 579, in _callTestMethod
-#     if method() is not None:
-#        ^^^^^^^^
-#   File "/Users/Jedi/Desktop/Project_IT6039/test.py", line 10, in testGutterGame
-#     self.game.rolls(0)
-# TypeError: 'list' object is not callable
     def testAllOnes(self):
-#         test.TestBowlingGame.testAllOnes failed with error: <class 'AttributeError'> 'TestBowlingGame' object has no attribute 'rollMany'
-# Traceback (most recent call last):
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 57, in testPartExecutor
-#     yield
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 623, in run
-#     self._callTestMethod(testMethod)
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 579, in _callTestMethod
-#     if method() is not None:
-#        ^^^^^^^^
-#   File "/Users/Jedi/Desktop/Project_IT6039/test.py", line 25, in testAllOnes
-#     self.rollMany(1, 20)
-#     ^^^^^^^^^^^^^
-# AttributeError: 'TestBowlingGame' object has no attribute 'rollMany'
         self.rollMany(1, 20)
         assert self.game.score()==20
 
     def testOneSpare(self):
-#         test.TestBowlingGame.testOneSpare failed with error: <class 'TypeError'> 'list' object is not callable
-# Traceback (most recent call last):
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 57, in testPartExecutor
-#     yield
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 623, in run
-#     self._callTestMethod(testMethod)
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 579, in _callTestMethod
-#     if method() is not None:
-#        ^^^^^^^^
-#   File "/Users/Jedi/Desktop/Project_IT6039/test.py", line 42, in testOneSpare
-#     self.game.rolls(5)
-# TypeError: 'list' object is not callable
         self.game.rolls(5)
         self.game.rolls(5)      
         self.game.rolls(3)
@@ -82,36 +41,10 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score()==24
 
     def testPerfectGame(self):
-#         test.TestBowlingGame.testPerfectGame failed with error: <class 'AttributeError'> 'TestBowlingGame' object has no attribute 'rollMany'
-# Traceback (most recent call last):
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 57, in testPartExecutor
-#     yield
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 623, in run
-#     self._callTestMethod(testMethod)
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 579, in _callTestMethod
-#     if method() is not None:
-#        ^^^^^^^^
-#   File "/Users/Jedi/Desktop/Project_IT6039/test.py", line 80, in testPerfectGame
-#     self.rollMany(10,12)
-#     ^^^^^^^^^^^^^
-# AttributeError: 'TestBowlingGame' object has no attribute 'rollMany'
         self.rollMany(10,12)
         assert self.game.score()==300
     
     def testOneSpare(self):
-#         test.TestBowlingGame.testOneSpare failed with error: <class 'AttributeError'> 'TestBowlingGame' object has no attribute 'rollMany'
-# Traceback (most recent call last):
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 57, in testPartExecutor
-#     yield
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 623, in run
-#     self._callTestMethod(testMethod)
-#   File "/opt/homebrew/Cellar/python@3.11/3.11.6_1/Frameworks/Python.framework/Versions/3.11/lib/python3.11/unittest/case.py", line 579, in _callTestMethod
-#     if method() is not None:
-#        ^^^^^^^^
-#   File "/Users/Jedi/Desktop/Project_IT6039/test.py", line 97, in testOneSpare
-#     self.rollMany(5,21)
-#     ^^^^^^^^^^^^^
-# AttributeError: 'TestBowlingGame' object has no attribute 'rollMany'
         self.rollMany(5,21)
         assert self.game.score()==150
 
