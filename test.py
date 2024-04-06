1c4adf100a946430271a1557a63e971832bf1f8e
jedi24garcia <jedi24garcia@gmail.com>
2024-04-06 23:27:26 +1300

    renamed variable to follow PEP 8 guideline

--- test.py
+++ test.py
@@ -6,36 +6,36 @@ class TestBowlingGame(unittest.TestCase):
         self.game = BowlingGame() 
 
     def testGutterGame(self): 
-        self.rollMany(0, 20)
+        self.roll_many(0, 20)
         self.assertEqual(self.game.score(), 0)
 
     def testAllOnes(self):
-        self.rollMany(1, 20)
+        self.roll_many(1, 20)
         self.assertEqual(self.game.score(), 20)
 
     def testOneSpare(self):
         self.game.roll(5)
         self.game.roll(5)      
         self.game.roll(3)
-        self.rollMany(0, 17)
+        self.roll_many(0, 17)
         self.assertEqual(self.game.score(), 16)
 
     def testOneStrike(self):
         self.game.roll(10)
         self.game.roll(4)
         self.game.roll(3)
-        self.rollMany(0, 16)
+        self.roll_many(0, 16)
         self.assertEqual(self.game.score(), 24)
 
     def testPerfectGame(self):
-        self.rollMany(10, 12)
+        self.roll_many(10, 12)
         self.assertEqual(self.game.score(), 300)
     
     def testOneSpare(self):
-        self.rollMany(5, 21)
+        self.roll_many(5, 21)
         self.assertEqual(self.game.score(), 150)
 
-    def rollMany(self, pins,rolls):
+    def roll_many(self, pins,rolls):
         for i in range(rolls):
             self.game.roll(pins)
 
